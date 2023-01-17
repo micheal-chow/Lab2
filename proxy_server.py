6bc6d19eb88096729afece83147082d33981ee10
Micheal Chow <micheal.chow672@gmail.com>
2023-01-16 18:36:45 -0700

    implemented multi-threading proxy server

--- proxy_server.py
+++ proxy_server.py
@@ -73,7 +73,7 @@ def start_threaded_server():
         while True:
             conn, addr = server_socket.accept()
             thread = Thread(target=handle_connection, args=(conn,addr))
-
+            thread.run()
     
 
 # start_server()
